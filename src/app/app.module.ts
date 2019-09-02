38c7efe91d5fc43ef82e016fbfdce3b762aa1648
Romaric Benoit <carp.philippe@gmail.com>
2019-09-02 16:43:33 +0200

    Create Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    UiModule,
+    LoginModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
